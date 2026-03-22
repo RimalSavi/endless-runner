99923614abded5bd8527bc2f5d615dac82846bf1
RimalSavi <rimalsavi@gmail.com>
2026-03-22 18:11:14 +0100

    Clean Vite template and add game structure

---
+++ src/components/Game.tsx
@@ -0,0 +1,9 @@
+const Game = () => {
+    return (
+        <div style={{width: '100vw', height: '100vh'}}>
+            <h1 style={{width: '100vw', height: '100vh'}}>Endless Runner</h1>
+        </div>
+    )
+}
+
+export default Game
